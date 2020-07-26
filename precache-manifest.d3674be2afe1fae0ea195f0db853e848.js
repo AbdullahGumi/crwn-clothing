b924e321c5c69b9a90716c64c380b66d84b19a8f
abdullah gumi <abdulgumi77@gmail.com>
2020-07-26 15:36:58 +0100

    Updates

---
+++ precache-manifest.d3674be2afe1fae0ea195f0db853e848.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d38e55aca0ac73cdd4a9b499620501a",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "fb4df18cb0185d0d3a36",
+    "url": "/crwn-clothing/static/css/main.1511587b.chunk.css"
+  },
+  {
+    "revision": "48689cfdb26f889b9d15",
+    "url": "/crwn-clothing/static/js/2.54508520.chunk.js"
+  },
+  {
+    "revision": "fb4df18cb0185d0d3a36",
+    "url": "/crwn-clothing/static/js/main.d4e99fda.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
